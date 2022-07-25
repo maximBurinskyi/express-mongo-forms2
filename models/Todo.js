99d2e8e2d0d4191106b31c61097a59337583ea04
maximBurinskyi <maxim.burinskyi@gmail.com>
2022-07-26 00:00:01 +0300

    first commit

---
+++ models/Todo.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+
+const schema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = model('Todo', schema);
